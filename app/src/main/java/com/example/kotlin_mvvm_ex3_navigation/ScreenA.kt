4b1194f11901b238d2955d7466e84c7f0fab7bd6
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-05-16 13:09:00 +0800

    Update pages basic UI.

--- app/src/main/java/com/example/kotlin_mvvm_ex3_navigation/ScreenA.kt
+++ app/src/main/java/com/example/kotlin_mvvm_ex3_navigation/ScreenA.kt
@@ -1,2 +1,24 @@
 package com.example.kotlin_mvvm_ex3_navigation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScreenA() {
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Screen A")
+        Button(onClick = {}) {
+            Text(text = "Go to Screen B")
+        }
+    }
+}
