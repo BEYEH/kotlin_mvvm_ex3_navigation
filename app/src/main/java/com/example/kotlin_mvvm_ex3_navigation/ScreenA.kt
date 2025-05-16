2b7103afaced8babe7cc9127d43fb318b15761bc
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-05-16 13:38:13 +0800

    Switch to screen b.

--- app/src/main/java/com/example/kotlin_mvvm_ex3_navigation/ScreenA.kt
+++ app/src/main/java/com/example/kotlin_mvvm_ex3_navigation/ScreenA.kt
@@ -8,16 +8,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 
 @Composable
-fun ScreenA() {
+fun ScreenA(navController: NavController) {
     Column(
         Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(text = "Screen A")
-        Button(onClick = {}) {
+        Button(onClick = {
+            navController.navigate("screen_B")
+        }) {
             Text(text = "Go to Screen B")
         }
     }
