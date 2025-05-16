9484f0f07947f53017b0fb2d060c781345404ff2
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-05-16 10:59:07 +0800

    Create app.

---
+++ app/src/main/java/com/example/kotlin_mvvm_ex3_navigation/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.kotlin_mvvm_ex3_navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlin_mvvm_ex3_navigation.ui.theme.Kotlin_mvvm_ex3_navigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Kotlin")
+            }
+        }
+    }
+}
